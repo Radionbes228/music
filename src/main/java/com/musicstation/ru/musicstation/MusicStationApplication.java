7fb8c39a23f829baeb71f1dd1f73cc2ff79eea56
radion <radionbes@gmail.com>
2023-12-23 22:29:59 +0300

    Create project

---
+++ src/main/java/com/musicstation/ru/musicstation/MusicStationApplication.java
@@ -0,0 +1,13 @@
+package com.musicstation.ru.musicstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicStationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musicstation/ru/musicstation/MusicStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicstation.ru.musicstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
